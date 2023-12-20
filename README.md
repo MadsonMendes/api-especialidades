56f9afe93d2c04be817efd10c3230641ad3cd899
Madson <madsonyt@gmail.com>
2023-12-20 15:34:50 -0300

    (#3) Corrigido mobile

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # API de Especialidades Escoteiras
 
-Uma API simples que retorna itens e requisitos de uma especialidade escoteira desejada. Criada como um estudo utilizando Flask, BeautifulSoup e Next.js.
+Uma API simples que retorna itens e requisitos de uma especialidade escoteira desejada. Criada como um estudo utilizando Flask, BeautifulSoup e Next.js. Você pode testar a API pelo link: https://api-especialidades.vercel.app/
+
+## Como funciona
+
+- Faça uma requisição `GET` pelo endpoint `"api-especialidades.vercel.app/api/especialidades/<nome>"`
+
+- O script fara um scrapping da página, retornando as seguintes informações
+
+```json
+{
+  "titulo": "Anime",
+  "img": "https://www.escoteiros.org.br/wp-content/uploads/2020/06/Anime-e1592696526169.png",
+  "itens": {
+    "1": "Escolher 1 (um) anime de sua preferência e apresentá-lo, discorrendo sobre sua trama, personagens, criadores, artistas envolvidos, época de criação, etc.",
+    "2": "Realizar uma apresentação sobre a história dos animes, destacando seu surgimento, o desenvolvimento no país de origem, os principais títulos, os principais autores e artistas e sua chegada e exibição no Brasil.",
+    "...": "..."
+  }
+}
+```
 
 ## Stack
 
--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center w-screen gap-4">
-        <section className="w-1/2">
+        <section className="w-1/2 max-sm:w-full">
           <h1 className="mb-4 text-4xl font-extrabold">
             API de Especialidades Escoteiras
           </h1>
@@ -32,11 +32,11 @@ export default function Home() {
             </b>
           </p>
         </section>
-        <section className="w-1/2 grid gap-4">
+        <section className="w-1/2 grid gap-4 max-sm:w-full">
           <div className="flex space-x-5 justify-center">
             <input
               placeholder="Insira o nome da especialidade que você deseja"
-              className="w-5/6 px-4 py-2 border border-slate-200 bg-transparent text-slate-200 rounded-md	"
+              className="w-5/6 max-sm:w-6/8 px-4 py-2 border border-slate-200 bg-transparent text-slate-200 rounded-md	"
               type="text"
               value={Search}
               onChange={(e) => {
@@ -44,14 +44,14 @@ export default function Home() {
               }}
             ></input>
             <button
-              className="w-1/6 bg-violet-700 hover:bg-violet-800 duration-300 rounded-md	"
+              className="w-1/6 max-sm:w-2/8 bg-violet-700 hover:bg-violet-800 duration-300 rounded-md	"
               onClick={handleClick}
             >
               Solicitar
             </button>
           </div>
           <ReactSyntaxHighlighter
-            className="flex space-x-5 justify-center h-80 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-zinc-700/50 scrollbar-thumb-rounded-md"
+            className="flex space-x-5 justify-center h-80 "
             language="json"
             style={anOldHope}
             showLineNumbers
